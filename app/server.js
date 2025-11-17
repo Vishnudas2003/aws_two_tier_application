83c5f7a076a2d618b2c97584b1cd556d7d14e292
Vishnudas2003 <vishnudas7412@gmail.com>
2025-11-17 15:34:55 -0500

    Edited the Server.js for AWS ECS

--- app/server.js
+++ app/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
@@ -10,31 +9,9 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-// MySQL connection configuration
-function connectWithRetry() {
-  const db = mysql.createConnection({
-    host: process.env.DB_HOST || 'db',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'rootpassword',
-    database: process.env.DB_NAME || 'testdb'
-  });
-
-  db.connect((err) => {
-    if (err) {
-      console.error('❌ Database not ready, retrying in 5 seconds...', err.message);
-      setTimeout(connectWithRetry, 5000);
-    } else {
-      console.log('✅ Connected to MySQL database.');
-      global.db = db;
-    }
-  });
-}
-
-connectWithRetry();
-
 //routing path
 app.get('/', (req, res) => {
-  res.send('Hello from my AWS Two-Tier App!');
+  res.send('Hello from my AWS Two-Tier App! - Server is running.');
 });
 
 // Start the server
